d5f6eea318bb87a05ed0c6e4ecd66552d3a402f0
anikape <ana.br@hotmail.com>
2022-08-29 11:10:44 -0300

    inicio do desenvolvimento em mobile first

---
+++ script.js
@@ -0,0 +1,14 @@
+//Função para o menu hamburguer
+
+function clickMenu() {
+    //se #lista estiver com display igual a block, vai receber none
+    if (lista.style.display == 'block') {
+        lista.style.display = 'none'
+    } else {
+        lista.style.display = 'block'
+    }
+
+
+}
+
+console.log("oi")
